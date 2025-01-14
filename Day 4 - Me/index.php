e91d04c32125d848969a42e784a9f4250fd3cd2c
Ritesh <hajimeshanben45@gmail.com>
2025-01-14 14:17:18 +0530

    Added minimal styling and some other details

--- Day 4 - Me/index.php
+++ Day 4 - Me/index.php
@@ -7,12 +7,27 @@
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+    <?php
+        function printArray($array){
+            echo "[";
+            $len = count($array);
+            for($i=0; $i<$len; $i++){
+                if($i == ($len - 1))
+                    echo "$i";
+                else
+                echo "$i, ";
+            }
+            echo "]";
+        }
+    ?>
         <!-- All about Arrays! -->
         <div>
             <h3>For Loops in Array</h3>
             <?php
                 $a = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
-                echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                echo "Given array: ";
+                printArray($a);
+                echo "<br>";
                 for ($i = 0; $i < 10; $i++) {
                     echo $a[$i] ."<br>";
                 }
@@ -35,6 +50,8 @@
             <?php
                 // $a = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
                 // echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                echo "Given array: ";
+                printArray($a); 
                 echo "<p>After Var Dump -> </p>";
                 var_dump($a);
                 echo "<br>";
@@ -44,7 +61,35 @@
         <h3>Length of an Array</h3>
             <?php
                 // $a = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
-                echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                // echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                echo "Given array: ";
+                printArray($a);
+                echo "Length of Array: ".count($a);
+                echo "<br>";
+            ?>
+        </div>
+        <div>
+        <h3>Ascending order sorting</h3>
+            <?php
+                // $a = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
+                // echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                echo "Given array: ";
+                printArray($a); 
+                sort($a);
+                echo "<br>";
+                echo "After ascending sort: "; 
+                printArray($a);
+                echo "<br>";
+            ?>
+        </div>
+        <div>
+        <h3>Descending order sorting</h3>
+            <?php
+                // $a = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
+                // echo "<p>Given array: [1,2,3,4,5,6,7,8,9,10,11,12,13]</p>";
+                rsort($a);
+                echo "After descending sort: ";
+                printArray($a);
                 echo "<br>";
             ?>
         </div>
